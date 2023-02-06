bcbef4d3d221ae13980798114365dabdc81c7c14
Caio Basso <caio.basso27@gmail.com>
2023-02-06 17:00:23 -0300

    Setting up chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  dark: {
+    900: '#1A1A1A',
+    800: '#333333',
+    700: '#4D4D4D',
+    600: '#666666',
+    500: '#808080',
+    400: '#969696',
+    300: '#ADADAD',
+    200: '#C4C4C4',
+    100: '#DBDBDB',
+    50: '#F2F2F2',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript, theme } from '@chakra-ui/react';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
