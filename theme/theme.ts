f599b34769862fa241a41d4df48dfd239ba08cd8
Caio Basso <caio.basso27@gmail.com>
2023-02-08 12:08:31 -0300

    Refactor theme directory

---
+++ theme/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+    colors: {
+        dark: {
+            900: '#1A1A1A',
+            800: '#333333',
+            700: '#4D4D4D',
+            600: '#666666',
+            500: '#808080',
+            400: '#969696',
+            300: '#ADADAD',
+            200: '#C4C4C4',
+            100: '#DBDBDB',
+            50: '#F2F2F2',
+        },
+    }
+})
